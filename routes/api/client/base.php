b239ec733bbf83877afee72a64f15cb1e600cca7
justseia <seiitmurat.bekmuratuly@gmail.com>
2024-09-30 13:00:56 +0500

    update

--- routes/api/client/base.php
+++ routes/api/client/base.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 include __DIR__ . '/auth/auth.php';
 
 Route::middleware(['auth:sanctum', 'check.close.all.content', 'action.log.activity'])->name('api.')->group(function () {
-//Route::middleware(['check.close.all.content', 'action.log.activity'])->name('api.')->group(function () {
 
     include __DIR__ . '/home/home.php';
     include __DIR__ . '/online-analyze/online-analyze.php';
